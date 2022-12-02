b832aaea9e738f0894495e35c836d4614e4b73b9
NelliBilovol <nellibilovol44@gmail.com>
2022-12-02 18:44:01 +0200

    fix styles

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
 import styled from './Modal.module.scss';
 
 interface IProps {
@@ -15,7 +16,9 @@ const Modal: FC<IProps> = ({ children, closeModal }) => {
   return (
     <div className={styled.backdrop}>
       <div className={styled.modalContent}>
-        <div onClick={() => closeModal()}>X</div>
+        <div onClick={() => closeModal()} className={styled.closeBtn}>
+          <AiOutlineClose />
+        </div>
         {children}
         <button onClick={() => loginSubmit()}>Login!</button>
       </div>
